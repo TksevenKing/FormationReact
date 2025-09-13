9d094cd307d7d03ac5718927bd7cb0ecf498be0c
oumarKingDev <oumarciss300@gmail.com>
2025-09-13 19:22:06 +0100

    Ajout de Footer avec controle de l'email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,22 @@
 import { useState } from "react"
+import '../styles/Footer.css'
 
-function cheickValue(value){
-    // const inputValue = value
-    return (
-        <div>test</div>
-    )
-}
+
 function Footer() {
     const [inputValue, setInputValue] = useState("Entrez votre mail")
+
+    function cheickValue(){
+        if (!inputValue.includes('@')) {
+            alert("Attention il n'y pas '@' ceci n'est pas une addresse mail valide")
+        }
+    }
     return (
         <footer className="footer">
             <div>Pour les passionnee de plantes 🌿🌱🌵</div>
             <div>Laissez-nous votre mail :</div>
-            <input type="text" value={inputValue} 
+            <input type="text" placeholder="Entrez votre email" 
                 onChange={ (e) => setInputValue(e.target.value)}
-                onBlur={() => cheickValue(inputValue)}>
+                onBlur={cheickValue}>
             </input>
             
             
